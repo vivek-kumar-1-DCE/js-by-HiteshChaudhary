7546c5704248bf1c8c804e86fc7854c9b66047a9
Vivek Kumar <vivek.jp1903@gmail.com>
2025-11-01 14:26:08 +0000

    Strins

---
+++ .devcontainer/01_basics/string.js
@@ -0,0 +1,11 @@
+const name = "vivek kaumar"
+const rollNo = 62
+
+// console.log(`my name is ${name} and my roll number is ${rollNo} `);
+
+const Gamename = new String("cricekt")  //another method to declare string using String constructor
+//     name of string        value     
+console.log(Gamename[0]); 
+console.log(Gamename.__proto__); 
+console.log(Gamename.length);
+console.log(Gamename.toUpperCase());      
