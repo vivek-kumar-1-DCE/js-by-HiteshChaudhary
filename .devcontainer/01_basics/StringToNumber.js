5b2b5cb909c28f793429edfb23d32a8e5e24aeb8
Vivek Kumar <vivek.jp1903@gmail.com>
2025-11-01 09:58:42 +0000

    confusingConversions

---
+++ .devcontainer/01_basics/StringToNumber.js
@@ -0,0 +1,17 @@
+// console.log(2**3);
+// let str1 = "vivek "
+// let str2 = "Kumar"
+// console.log(str1 +str2);
+//---------------------------inmportant------------------------
+let str3 ="10"
+let num1 =5
+/*
+console.log(str3 + num1);  //105
+console.log( "1" + 2+ 5 ); //125
+console.log( 1 + 2 + "5" ); //35
+console.log( "10" - 5 );   //5
+*/
+console.log(+true);
+
+
+
