3bdce7e72bbd1472c81fdf371b4ae56d8da39413
Vivek Kumar <vivek.jp1903@gmail.com>
2025-11-02 04:03:58 +0000

    maths and number

---
+++ .devcontainer/numsANdMaths.js
@@ -0,0 +1,15 @@
+const count = 100
+// console.log(count);
+
+const age = new Number(25)  //declaration of a Number object
+// console.log(typeof age);
+// console.log(age);
+// console.log(typeof age.toString());
+// console.log(age.toString().length);
+// console.log(typeof age);
+
+
+
+// ----------------------------------Maths----------------------------------
+
+console.log(Math)
