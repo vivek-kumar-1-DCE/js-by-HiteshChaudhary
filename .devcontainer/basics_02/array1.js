04ddb0ea27d3e121bb29ab17a9a50e9d3b2bba5a
Vivek Kumar <vivek.jp1903@gmail.com>
2025-11-03 11:56:15 +0000

    array l-1

---
+++ .devcontainer/basics_02/array1.js
@@ -0,0 +1,34 @@
+const array = [0,1,2,3,4]
+const arr = ["vivek", "shaktiman"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr2[0]);
+
+//array methods
+ 
+// array.push(9)
+// console.log(array);
+// array.pop()
+// console.log(array);
+// array.unshift(15)
+// console.log(array);
+// array.shift()
+// console.log(array);
+
+// console.log(arr.includes(4));
+// console.log(array.indexOf(9));
+
+// const newarr = array.join()    cconvert in string
+// console.log(newarr);
+// console.log(array);
+
+//slice  and splice
+
+console.log("A",array);
+console.log(array.slice(1,3));
+console.log("after slice",array);
+console.log("A",array);
+console.log(array.splice(1,3));
+console.log("after splice",array);
+
+
+
